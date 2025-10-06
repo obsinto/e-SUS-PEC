7ff8bc8a2c89957ea669d66d74bbef0da12c7399
Deyvid <deyvidsant@hotmail.com>
2025-10-06 14:55:26 -0300

    fix

--- install.sh
+++ install.sh
@@ -5,9 +5,9 @@ RED='\033[0;31m'
 NC='\033[0m' # No Color
 ARGS=""
 
-# Verificando variáveis de ambiente
-if [ -n "$POSTGRES_HOST" ] && [ -n "$POSTGRES_PORT" ] && [ -n "$POSTGRES_DB" ]; then
-    DB_URL="jdbc:postgresql://$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB"
+# Verificando variáveis de ambiente (usando DB_* ao invés de POSTGRES_*)
+if [ -n "$DB_HOST" ] && [ -n "$DB_PORT" ] && [ -n "$POSTGRES_DB" ]; then
+    DB_URL="jdbc:postgresql://$DB_HOST:$DB_PORT/$POSTGRES_DB"
 fi
 
 # Echo das variáveis de ambiente
@@ -16,29 +16,28 @@ echo "Variáveis de ambiente:"
 echo "*******************"
 echo "HTTPS_DOMAIN: ${HTTPS_DOMAIN}"
 echo "DB_URL: ${DB_URL}"
-echo "POSTGRES_USER: ${POSTGRES_USER}"  
-echo "POSTGRES_PASS: ${POSTGRES_PASS}"
+echo "DB_USER: ${DB_USER}"  
+echo "DB_PASSWORD: ${DB_PASSWORD}"
 echo "JAR_FILENAME: ${JAR_FILENAME}"
 echo "TRAINING: ${TRAINING}"
 echo "*******************\n\n${NC}"
 
-
-# Verificando variável de certificado https
-if [ -n "$HTTPS_DOMAIN" ]; then
-  ARGS="$ARGS -cert-domain=${HTTPS_DOMAIN}"
-fi
+# NÃO GERAR CERTIFICADO - Traefik cuida disso
+# if [ -n "$HTTPS_DOMAIN" ]; then
+#   ARGS="$ARGS -cert-domain=${HTTPS_DOMAIN}"
+# fi
 
 # Verificando variáveis de banco de dados
 if [ -n "$DB_URL" ]; then
   ARGS="$ARGS -url=${DB_URL}" 
 fi
 
-if [ -n "$POSTGRES_USER" ]; then
-  ARGS="$ARGS -username=${POSTGRES_USER}"
+if [ -n "$DB_USER" ]; then
+  ARGS="$ARGS -username=${DB_USER}"
 fi
 
-if [ -n "$POSTGRES_PASS" ]; then  
-  ARGS="$ARGS -password=${POSTGRES_PASS}"
+if [ -n "$DB_PASSWORD" ]; then  
+  ARGS="$ARGS -password=${DB_PASSWORD}"
 fi
 
 # A ser executado java -jar
@@ -49,17 +48,16 @@ echo "*******************\n\n${NC}"
 # Executa o comando
 java -jar ${JAR_FILENAME} -console ${ARGS} -continue
 
-
 # Verificando se a variável de treinamento existe, caso sim, executa o SQL
 if [ -n "$TRAINING" ]; then
   echo -e "${GREEN}Treinamento habilitado. Executando SQL de configuração...${NC}"
-  PSQL_CMD="psql -h ${POSTGRES_HOST} -p ${POSTGRES_PORT} -U ${POSTGRES_USER} -d ${POSTGRES_DB} -c \"update tb_config_sistema set ds_texto = null, ds_inteiro = 1 where co_config_sistema = 'TREINAMENTO';\""
   
   # Exporta a senha do banco para evitar o prompt
-  export PGPASSWORD="${POSTGRES_PASS}"
+  export PGPASSWORD="${DB_PASSWORD}"
 
-  # Executa o comando SQL
-  eval $PSQL_CMD
+  # Executa o comando SQL (usando DB_* ao invés de POSTGRES_*)
+  psql -h ${DB_HOST} -p ${DB_PORT} -U ${DB_USER} -d ${POSTGRES_DB} -c "update tb_config_sistema set ds_texto = null, ds_inteiro = 1 where co_config_sistema = 'TREINAMENTO';"
+  
   if [ $? -eq 0 ]; then
     echo -e "${GREEN}Configuração de treinamento aplicada com sucesso.${NC}"
   else
